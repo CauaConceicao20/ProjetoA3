7abdf374f3074f2fcd990ae94a8ce06edf069315
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-03-27 22:41:43 -0300

    adicionando test

---
+++ Projeto/src/test/java/com/projeto/calculadora/controller/test/CalculadoraControllerTest.java
@@ -0,0 +1,12 @@
+package com.projeto.calculadora.controller.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraControllerTest {
+	@Test
+	public void testAdicao() {
+		assertTrue(true);
+	}
+}
