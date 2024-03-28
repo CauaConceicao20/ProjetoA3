dbc56c0b283fcb36cf939df0421433dc383f9182
Cauã Conceição <95243694+CauaConceicao20@users.noreply.github.com>
2024-03-28 01:37:01 -0300

    build: create release.config.js

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['develop'], // Branch que dispara o processo de release
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+      },
+    ],
+  ],
+};
