f583f04484a508d4b689e1c41ccff6be401e7f60
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-03-29 13:26:07 -0300

    feat: adiciona funcionalidade de soma

---
+++ Projeto/src/main/java/com/projeto/calculadora/controller/OperacoesController.java
@@ -0,0 +1,14 @@
+package com.projeto.calculadora.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/operacao")
+public class OperacoesController {
+	
+	public int soma(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+	}
+}
