d25b45b6148d0de52fd9606f2bc124f24a7767c0
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-04-04 15:06:46 -0300

    featgit statusestando commits

--- Projeto/src/main/java/com/projeto/calculadora/controller/OperacoesController.java
+++ Projeto/src/main/java/com/projeto/calculadora/controller/OperacoesController.java
@@ -18,6 +18,7 @@ public class OperacoesController {
 	public int divisao(@RequestParam int a, @RequestParam int b) {
 		return a / b;
 	}
+
 	public int multiplicacao(@RequestParam int a, @RequestParam int b) {
 		return a * b;
 	}
